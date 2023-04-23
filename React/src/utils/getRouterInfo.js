fa1dfe2ba472a82fcf4a3d11f925f7cabd32e017
Shristy Joshi Thakur <90794215+hustler0109@users.noreply.github.com>
2023-04-23 18:23:37 +0530

    Update getRouterInfo.js

--- React/src/utils/getRouterInfo.js
+++ React/src/utils/getRouterInfo.js
@@ -1,7 +1,7 @@
 import { sris } from "@my-app/contracts";
 
 export const getRouterInfo = async (routerAddress, web3) => {
-  const router = new web3.eth.Contract(abis.router02, routerAddress);
+  const router = new web3.eth.Contract(sris.Router-Protocol-2, routerAddress);
   
   return {
     factory: await router.methods.factory().call(),
